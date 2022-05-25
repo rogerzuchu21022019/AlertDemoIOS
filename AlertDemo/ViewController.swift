6c016fe6607bbe42a2dca930f2c3fdbd3e6efa45
rogerzuchu21022019 <97218806+rogerzuchu21022019@users.noreply.github.com>
2022-05-25 10:24:08 +0700

    Initial Commit

--- AlertDemo/ViewController.swift
+++ AlertDemo/ViewController.swift
@@ -8,7 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var myLabel: UILabel!
+    @IBAction func Press(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Alert", message: "Choose", preferredStyle: .alert)
+        let actionAgree1 = UIAlertAction(title: "Agree1", style: .default){(action) in
+            self.myLabel.text = "Agree"
+        }
+        let actionAgree2 = UIAlertAction(title: "Agree2", style: .destructive){(action) in
+            self.myLabel.text = "Agree"
+        }
+        
+        let actionCancel = UIAlertAction(title: "Cancel", style: .cancel){
+            (action) in
+            self.myLabel.text = "Cancel"
+        }
+        alert.addAction(actionAgree1)
+        alert.addAction(actionAgree2)
+        alert.addAction(actionCancel)
+        self.present(alert, animated: true, completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
